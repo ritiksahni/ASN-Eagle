2a0f1d803110668420a6496180e2d653c642f6aa
Tech Hacker <anshbhawnani@gmail.com>
2020-02-19 21:31:19 +0530

    A couple of changes and error fixes have been made:
    1. Added command line arguments over the interactive method, users have to enter the domain name necessarily, and calculating the IP ranges and writing them to a file are optional.
    2. Operations have been made verbose
    3. Users no longer have to enter the AS number again for finding the netblocks.
    4. New dependency has been added,i.e., the module argparse
    5. Minor code optimizations and bug fixes

--- ASN-Eagle.py
+++ ASN-Eagle.py
@@ -6,6 +6,7 @@ import sys
 import terminal_banner
 from termcolor import colored
 import os
+import argparse
 
 os.system('clear')
 
@@ -25,42 +26,78 @@ Twitter: https://twitter.com/RitikSahni22
 Instagram: https://www.instagram.com/deep.tech
 """)
 
+
 # Print Banner for ASN-Eagle
 banner_terminal = terminal_banner.Banner(banner_text)
 print (colored(banner_terminal, 'cyan'))
 
+help = 'python3 ASN-Eagle.py -d domain [OPTIONS]\nOPTIONS:\n\n-i\t--ip-range\tDiscover Netblocks/IP ranges\n-o\t--output-file\tWrite output to a file\n'
+
+arg_parse = argparse.ArgumentParser(usage=help)
+arg_parse.add_argument('-d','--domain',required=True)
+arg_parse.add_argument('-i','--ip-ranges', default=False, action='store_const',const = 1)
+arg_parse.add_argument('-o','--output-file')
+args = arg_parse.parse_args()
+
 # Fetch IP address of website using socket and save it to a variable
-hostname = input(colored("Enter hostname [e.g google.com, tesla.com]: ", 'green'))
+#hostname = input(colored("Enter hostname [e.g google.com, tesla.com]: ", 'green'))
+hostname = args.domain
 ip_addr = socket.gethostbyname(hostname)
+
+print(colored("Fetching AS Number..\n", 'white')) 
 
 # Extract ASN using ipinfo API.
 asn_fetch = requests.get('https://ipinfo.io/'+ip_addr+'/org?token=c8bb8b5ed87127')
 
-print(colored("ASN Details found!!\n\n"+asn_fetch.text, 'magenta')) # Print ASN details
+#fetch the desired result and store it
+discovered_asn = asn_fetch.text
+
+print(colored("[+] ASN Details found!!\n", 'magenta')) # Print ASN details
+print(colored(discovered_asn , 'yellow')) 
 access_rights = (0o755) # Defining access rights for creating output directory.
 
-prompt1 = input(colored("\n\nDo you want to scan for IP ranges from discovered ASN? (Y or N)\n", 'green')) # Prompt for IP ranges
+#prompt1 = input(colored("\nDo you want to scan for IP ranges from discovered ASN? (Y or N)\n", 'green')) # Prompt for IP ranges
 
 path = ("./output")
 
-if prompt1 == "Y": # If-elif for saving IP ranges functionality.
+if(args.ip_ranges == 1):
+
+	#fetch the AS Number from the previos output
+	new_asn = discovered_asn.split(' ')[0]
+	
+	print(colored("Fetching IP ranges belonging to the AS..\n", 'white')) 
+		
 	try:
-		discovered_asn = input(colored('Enter the discovered ASN: ', 'green'))
-		result = requests.get('https://api.hackertarget.com/aslookup/?q='+discovered_asn) # Makes request to fetch IP RANGE LIST.
+		result = requests.get('https://api.hackertarget.com/aslookup/?q='+new_asn) # Makes request to fetch IP RANGE LIST.
+		print(colored("[+] IP ranges found!!\n", 'magenta'))
+		print(result.text+'\n') 
+		
 	except Exception:
-		print("Invalid ASN!")
-	filename = input(colored("\nEnter the filename for saving the results [e.g. results.txt] : ", 'blue'))
+		print("There is something wrong.")
+	
+
+if(args.output_file != None):
+		
+	filename = args.output_file
+
 	try: # Creates 'output/' directory and file to save result containing IP ranges.
 		os.mkdir(path, access_rights)
+		
 	except OSError:
 		print(colored("Output directory already exists, creating file with IP ranges!", 'green'))
+		
 	file = open("./output/"+filename, "w")
+
 	try:
-		file.write(result.text)
-		print (colored("\n\nResults saved in output/"+filename, 'blue'))
+		if(args.ip_ranges == 1):
+			file.write(result.text+'\n')
+		else:
+			file.write(discovered_asn+'\n')
+		print (colored("\nResults saved in output/"+filename+'\n', 'blue'))
+		
 	except Exception:
 		print (colored("Task failed! Try again!", 'red'))
-elif prompt1 == "N":
-	sys.exit
-else:
-	print (colored("Invalid input!", 'red'))
+
+
+
+
